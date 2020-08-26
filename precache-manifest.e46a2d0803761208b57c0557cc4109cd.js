3aaf45892af52b9b71805915f5cfab9fd5e6c17d
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2020-08-26 18:04:18 +0530

    Updates

---
+++ precache-manifest.e46a2d0803761208b57c0557cc4109cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b527e19e1b4dcbef9f8483588eac41b0",
+    "url": "/orgchart-council/index.html"
+  },
+  {
+    "revision": "f1e108680aeed014e720",
+    "url": "/orgchart-council/static/css/main.6f518fba.chunk.css"
+  },
+  {
+    "revision": "40584eb52b25b1c20315",
+    "url": "/orgchart-council/static/js/2.96b7ed8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/orgchart-council/static/js/2.96b7ed8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1e108680aeed014e720",
+    "url": "/orgchart-council/static/js/main.482d1cdc.chunk.js"
+  },
+  {
+    "revision": "cb8078c266d2b84631f8",
+    "url": "/orgchart-council/static/js/runtime-main.5a5a563f.js"
+  }
+]);
